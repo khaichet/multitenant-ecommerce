11c3a484dba9a7017ca7287e6de207556aa3f031
khaichet <khaichet1708@gmail.com>
2025-04-20 04:50:40 +0700

    02:Home

---
+++ src/app/(home)/contact/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div>
+      <p>Contact Page</p>
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>Foo</div>;
+};
+
+export default Page;

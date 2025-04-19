0ae27346010c90330376c779656641a85a61a3fb
khaichet <khaichet1708@gmail.com>
2025-04-20 06:50:01 +0700

    03: Payload

--- src/app/(app)/(home)/navbar.tsx
+++ src/app/(app)/(home)/navbar.tsx
@@ -45,7 +45,7 @@ const navbarItems = [
 
 const Navbar = () => {
   const currentPath = usePathname();
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   return (
     <nav className="h-20 flex border-b justify-between font-medium bg-white">
       <Link href="/" className="pl-6 flex items-center">
--- src/app/(home)/page.tsx
+++
@@ -1,5 +0,0 @@
-const Page = () => {
-  return <div>Foo</div>;
-};
-
-export default Page;

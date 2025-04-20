8e7a50ba64d7680161205f12c3382c77b16ca7ea
khaichet <khaichet1708@gmail.com>
2025-04-20 16:56:55 +0700

    04: Sidebar

---
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -0,0 +1,35 @@
+"use client"
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { ListFilterIcon, SearchIcon } from "lucide-react";
+import { CustomCategory } from "../types";
+import { CategoriesSidebar } from "./categories-sidebar";
+import { Button } from "@/components/ui/button";
+
+interface Props {
+  disabled?: boolean;
+  data: CustomCategory[]
+}
+export const SearchInPut = ({ disabled, data }: Props) => {
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+
+  return (
+    <div className="flex items-center gap-2 w-full">
+      <CategoriesSidebar data={data} open={isSidebarOpen} onOpenChange={setIsSidebarOpen} />
+      <div className="relative w-full">
+        <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 text-neutral-500" />
+        <Input
+          className="pl-8"
+          placeholder=" Search Products "
+          disabled={disabled}
+        />
+      </div>
+      <Button variant="elevated"
+        className="size-12 shrink-0 flex lg:hidden"
+        onClick={() => setIsSidebarOpen(true)}>
+        <ListFilterIcon />
+      </Button>
+    </div>
+  );
+};
